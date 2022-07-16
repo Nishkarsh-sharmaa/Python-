e61d95309f69f19e120339fff5b03a9c9e11d359
Nishkarsh-sharmaa <109418919+Nishkarsh-sharmaa@users.noreply.github.com>
2022-07-16 17:43:42 +0530

    Function.py

---
+++ Function.py
@@ -0,0 +1,13 @@
+import operator
+W= input('Please enter a string ')
+def most_frequent(string):
+      d = dict()
+      for key in string:
+         if key not in d:
+                d[key] = 1
+            else:
+                 d[key] += 1
+             
+       cd = sorted(d.itemd(),key=operator.itemgetter(1),reverse=True)
+       return cd
+       print (most_frequent(W))
